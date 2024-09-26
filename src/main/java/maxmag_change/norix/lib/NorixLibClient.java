1bfb8027d474567383418f2af9f41b8a70d5e432
magistr-of-code <maksimka-sulimov@mail.ru>
2024-09-26 19:38:27 +0300

    essential

--- src/main/java/maxmag_change/norix/lib/NorixLibClient.java
+++ src/main/java/maxmag_change/norix/lib/NorixLibClient.java
@@ -2,7 +2,7 @@ package maxmag_change.norix.lib;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class NorixClient implements ClientModInitializer {
+public class NorixLibClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
 
