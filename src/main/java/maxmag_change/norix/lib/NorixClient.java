fb7aa202a1b9fbd8f8741a6651be965fc730d38a
magistr-of-code <maksimka-sulimov@mail.ru>
2024-09-26 19:01:29 +0300

    initial

---
+++ src/main/java/maxmag_change/norix/lib/NorixClient.java
@@ -0,0 +1,10 @@
+package maxmag_change.norix.lib;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NorixClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/maxmag_change/norix/lib/NorixlibDataGenerator.java
@@ -0,0 +1,11 @@
+package maxmag_change.norix.lib;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NorixlibDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
